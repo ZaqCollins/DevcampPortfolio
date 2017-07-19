eba9e50b8989794869db5f04836d11699fde9c99
Zaq Collins <futurerm@hotmail.com>
2017-07-19 11:12:03 -0600

    Implemented image defaults for portfolio items

--- db/schema.rb
+++ db/schema.rb
@@ -5,7 +5,7 @@
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# from scratch. The Tlatter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
@@ -45,8 +45,7 @@ ActiveRecord::Schema.define(version: 20170718215621) do
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at", null: false    t.datetime "updated_at", null: false
   end
 
   create_table "skills", force: :cascade do |t|
