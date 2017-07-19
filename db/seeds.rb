affd77fecd2bd6daf870191ed8ad3bdf632710ff
Zaq Collins <futurerm@hotmail.com>
2017-07-19 12:20:25 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -47,5 +47,15 @@ end
 
 end
 
+
+
 puts "9 portfolio items created"
 
+3.times do |technology|
+  Portfolio.last.technologies.create!( 
+    name: "Technology #{technology}", 
+    portfolio_id: Portfolio.last.id
+ )
+end
+
+puts "3 technologies created"
