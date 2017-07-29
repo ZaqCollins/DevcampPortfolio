6ee2a17399de53ecdba1406be2943394398d80e4
Zaq Collins <futurerm@hotmail.com>
2017-07-29 15:06:03 -0600

    Implemented partials for blog, font awesome, and pagination.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,14 +5,14 @@ module ApplicationHelper
      " ".html_safe +
      (link_to "Login", new_user_session_path, class: style)
    else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style  
-     end  
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+     end
    end
-   
+
   def source_helper(layout_name)
     if session[:source]
       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
       content_tag(:p, greeting, class: "source-greeting")
-    end 
+    end
   end
-end 
+end
